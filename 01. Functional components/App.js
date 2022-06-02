c62c1f0c2012d823a4531982aa8d1ee5c94ad27b
fstipo <fstipanov@gmail.com>
2022-06-02 08:33:12 +0200

    Functional components

---
+++ 01. Functional components/App.js
@@ -0,0 +1,12 @@
+import Greet from './components/Greet';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Greet />
+    </div>
+  );
+}
+
+export default App;
---
+++ 01. Functional components/Greet.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Greet = () => <div>Hello Franko 😉</div>;
+
+export default Greet;
